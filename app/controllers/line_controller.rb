1df6417ef2648d064b55639aab836c0b3b4ad572
Chuang <33791604+chuang861012@users.noreply.github.com>
2019-01-16 00:00:20 +0800

    Add simple hentai scraper

--- app/controllers/line_controller.rb
+++ app/controllers/line_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'line/bot'
+require 'open-uri'
 
 class LineController < ApplicationController
   def webhook
@@ -12,7 +13,23 @@ class LineController < ApplicationController
     reply_token = params['events'][0]['replyToken']
     message = params['events'][0]['message']['text']
 
-    p message
+    response = nil
+
+    if message == '給點活路'
+      gallery = getGalleries.sample
+
+      text = {
+        type: 'text',
+        text: gallery[0]
+      }
+      image = {
+        type: 'image',
+        originalContentUrl: gallery[2],
+        previewImageUrl: gallery[2]
+      }
+
+      client.reply_message(reply_token, [text, image])
+    end
 
     response = {
       type: 'text',
@@ -22,5 +39,23 @@ class LineController < ApplicationController
     client.reply_message(reply_token, response)
 
     head :ok
+  end
+
+  private
+
+  def getGalleries
+    response = open('https://e-hentai.org/',
+                    'Host' => 'e-hentai.org',
+                    'User-Agent' => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
+                    'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+                    'Cookie' => ENV['HENTAI_COOKIE'])
+
+    html = Nokogiri::HTML(response, nil, 'UTF-8')
+
+    titles = html.xpath('//div[@class="id2"]/a/text()').map(&:inner_text)
+    links = html.xpath('//div[@class="id2"]/a/@href').map(&:inner_text)
+    thumbnail = html.xpath('//div[@class="id3"]/a/img/@src').map(&:inner_text)
+
+    titles.zip(links, thumbnail)
   end
 end
